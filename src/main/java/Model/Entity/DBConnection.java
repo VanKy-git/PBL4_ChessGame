a9393f905bf39e38e00c8e3c87003746378a103a
Nhuận Nguyễn Văn <102230205@sv1.dut.udn.vn>
2025-10-15 15:28:40 +0700

    sửa tên DataBase phù hợp và mật khẩu để chạy

--- src/main/java/Model/Entity/DBConnection.java
+++ src/main/java/Model/Entity/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 
 public class DBConnection {
-    private static HikariDataSource dataSource;
+    private final static HikariDataSource dataSource;
 
     static {
         HikariConfig config = new HikariConfig();
